02bded2ae796bf5898c0fd2adf4a76d067c4c131
João <arantesjoaov@gmail.com>
2022-06-19 14:43:08 -0300

    messages being sent

--- src/cliente.py
+++ src/cliente.py
@@ -1,13 +1,19 @@
+import json
+from base64 import b64encode
 from datetime import datetime
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Hash import SHA512
 
-from src.servidor import Servidor
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA512
+from Crypto.Protocol.KDF import PBKDF2
+
+from src.servidor import Servidor, User
 
 class Cliente:
   def __init__(self):
     self.usuarios = []
     self.servidor = Servidor()
+    self.codigo_2fa_usado_no_login = ''
+    self.pbkdf2_codigo_2fa_usado_no_login = ''
 
 
   def cadastrar(self):
@@ -50,7 +56,47 @@ class Cliente:
 
     salt = usuario[::-1]
     chave_PBKDF2 = PBKDF2(senha, salt, 32, count=10000, hmac_hash_module=SHA512)
-    # horario = str(datetime.now())[11:]
-    # horario_formatado = str(horario)[:8]
     horario_formatado = datetime.now()
-    self.servidor.login(usuario_buscado, chave_PBKDF2, horario_formatado)
+    sucesso_login = self.servidor.login(usuario_buscado, chave_PBKDF2, horario_formatado)
+    if sucesso_login != False:
+      self.codigo_2fa_usado_no_login = sucesso_login
+      self.iniciar_comunicacao(usuario_buscado)
+
+  def iniciar_comunicacao(self, usuario: User):
+    # Criando as chaves, salt e mensagem que serão utilizadas na criptografia
+    salt = usuario.nome_de_usuario[::-1]
+    print('\n')
+    codigo_pbkdf2 = PBKDF2(self.codigo_2fa_usado_no_login, salt, 32, count=10000, hmac_hash_module=SHA512)
+    self.pbkdf2_codigo_2fa_usado_no_login = codigo_pbkdf2
+
+    # Criando a interface de interação do usuário/cliente com o servidor
+    while True:
+      print('------------- Troca de mensagens ------------')
+      print('          Para sair, digite \'Sair\'')
+      print('---------------------------------------------')
+      print('\n')
+      message = input('Qual a mensagem a ser enviada? ')
+
+      if message.lower() == 'sair':
+        break
+
+      # Encriptografando a mensagem
+      cipher = AES.new(codigo_pbkdf2, AES.MODE_GCM)
+      header = b'header'
+      cipher.update(header)
+      b_message = message.encode('utf-8')
+      ciphertext, tag = cipher.encrypt_and_digest(b_message)
+
+      json_k = [ 'nonce', 'header', 'ciphertext', 'tag' ]
+      json_v = [ b64encode(x).decode('utf-8') for x in [cipher.nonce, header, ciphertext, tag ]]
+      mensagem_encriptografada = json.dumps(dict(zip(json_k, json_v)))
+
+      # Enviando mensagem pro servidor
+      print('\n')
+      print('Mensagem criptografada enviada para o servidor: ', mensagem_encriptografada)
+      print('\n')
+
+      self.servidor.receber_mensagem(mensagem_encriptografada)
+
+
+    
--- src/servidor.py
+++ src/servidor.py
@@ -1,11 +1,20 @@
+import time
+import json
+import pyotp
+import msvcrt
+
 import sys
 from time import sleep
+
+from base64 import b64decode
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA512
+from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Protocol.KDF import scrypt
+from Crypto.Util.Padding import unpad
+
 from datetime import datetime
 from datetime import timedelta
-import msvcrt
-import time
-import pyotp
 
 class User:
   def __init__(self, nome_de_usuario, hash_senha):
@@ -15,6 +24,8 @@ class User:
 class Servidor:
   def __init__(self):
     self.usuarios = []
+    self.codigo_2fa_usado_no_acesso = ''
+    self.pbkdf2_codigo_2fa_usado_no_acesso = ''
 
   def criptografar(self, usuario: str, token: str):
     salt = usuario[::-1]
@@ -57,22 +68,28 @@ class Servidor:
     if totp.verify(codigo_do_usuario):
       print('\n')
       print('Login feito com sucesso!')
-      self.iniciar_comunicacao()
+      print('\n')
+      self.codigo_2fa_usado_no_login = codigo_do_usuario
+      return codigo_do_usuario
     else:
       return False
 
-  def iniciar_comunicacao(self):
-    print('Comunicando...')
-
   def login(self, usuario: User, token: str, horario: str):
     hash_senha_digitada = self.criptografar(usuario.nome_de_usuario, token)
+    salt = usuario.nome_de_usuario[::-1]
 
     if usuario.hash_senha == hash_senha_digitada:
-      if self.gerar_2fa(horario) == False:
+      autenticacao = self.gerar_2fa(horario)
+      if autenticacao == False:
         print('\n')
         print('Código incorreto!')
+      else:
+        codigo_pbkdf2 = PBKDF2(self.codigo_2fa_usado_no_login, salt, 32, count=10000, hmac_hash_module=SHA512)
+        self.pbkdf2_codigo_2fa_usado_no_acesso = codigo_pbkdf2
+        return autenticacao
     else:
       print('A senha está incorreta.')
+      return
 
   def buscar_usuario(self, nome: str):
     for usuario in self.usuarios:
@@ -84,4 +101,21 @@ class Servidor:
     chave_scrypt = self.criptografar(nome, chave)
     usuario_a_cadastrar = User(nome, chave_scrypt)
     self.usuarios.append(usuario_a_cadastrar)
+
+  def receber_mensagem(self, mensagem):
+    ''' Descriptografia da mensagem enviada pelo cliente. '''
+    chave = self.pbkdf2_codigo_2fa_usado_no_acesso
+    try:
+      b64 = json.loads(mensagem)
+      json_k = [ 'nonce', 'header', 'ciphertext', 'tag' ]
+      jv = {k:b64decode(b64[k]) for k in json_k}
+
+      cipher = AES.new(chave, AES.MODE_GCM, nonce=jv['nonce'])
+      cipher.update(jv['header'])
+      mensagem_decifrada = cipher.decrypt_and_verify(jv['ciphertext'], jv['tag'])
+      print('A mensagem decifrada pelo servidor é:', mensagem_decifrada.decode("utf-8") )
+      print('\n')
+    except (ValueError, KeyError):
+      print('Ocorreu um erro na descriptografia')
+
 
